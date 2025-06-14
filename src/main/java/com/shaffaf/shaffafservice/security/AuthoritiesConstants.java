97a855c8cb11903251cd03d762ab6d8504dddd8d
mtauqeeriqbal <87613945+mtauqeeriqbal@users.noreply.github.com>
2025-06-14 13:33:56 +0500

    Added Seller, UnionHead, Union Member, Resident authoroties

--- src/main/java/com/shaffaf/shaffafservice/security/AuthoritiesConstants.java
+++ src/main/java/com/shaffaf/shaffafservice/security/AuthoritiesConstants.java
@@ -11,5 +11,15 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String SELLER = "ROLE_SELLER";
+
+    public static final String RESIDENT = "ROLE_RESIDENT";
+
+    public static final String UNION_MEMBER = "ROLE_UNION_MEMBER";
+
+    public static final String UNION_HEAD = "ROLE_UNION_HEAD";
+
+    public static final String EMPLOYEE = "ROLE_EMPLOYEE";
+
     private AuthoritiesConstants() {}
 }
